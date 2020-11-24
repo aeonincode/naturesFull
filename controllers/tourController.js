b74475e61c07025ceb7d6f65084e89131364062e
aeonincode <khashayar.sasannia@hotmail.com>
2020-11-24 20:14:48 +0100

    a better file structure

---
+++ controllers/tourController.js
@@ -0,0 +1,98 @@
+const fs = require('fs');
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+
+// 2) ROUTE HANDLERS
+exports.getAllTours = (req, res) => {
+    console.log(req.requestTime);
+    res.status(200).json({
+        status: 'success',
+        requestedAt: req.requestTime,
+        // useful when we sending an array,multiple object 
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+};
+
+exports.getTour = (req, res) => {
+    console.log(req.params);
+    exports. id = req.params.id *1;
+
+    exports. tour = tours.find(el => el.id === id)
+
+    // first solution
+    //if(id > tours.length) {
+
+    // second solution
+    if(!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    // first solution
+    // exports. tour = tours.find(el => el.id === id)
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    });
+}
+
+exports.createTour = (req,res) => {
+    //console.log(req.body);
+    exports.newId = tours[tours.length -1].id + 1;
+    exports.newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data : {
+                tour: newTour
+            }
+        })
+    })
+
+    //send response
+    //res.send('Done')
+}
+
+exports.updateTour = (req, res) => {
+    // with * covert it to number
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        // sendback data,updated tour
+        data: {
+            tour: '<Updated tour here...>'
+        }
+    })
+}
+
+exports.deleteTour = (req, res) => {
+    // with * covert it to number
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    // 204 means no content
+    res.status(204).json({
+        status: 'success',
+        // usually we dont send any data back, we send null to show that the resource that we deleted now no longer exists
+        data: null
+    });
+};

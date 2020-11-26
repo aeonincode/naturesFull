aec182c7bd5da92048c8c852a39520bca406769c
aeonincode <khashayar.sasannia@hotmail.com>
2020-11-26 17:14:16 +0100

    serving static files

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use(morgan('dev'));
 // simple middleware
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
     console.log('Hello from the middleware 🤘🏻');
     next();

bc7653aa1cbe8348e448e02842266608d010ed3e
aeonincode <khashayar.sasannia@hotmail.com>
2020-12-09 19:28:02 +0100

    Connecting Our database with the express App

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
+mongoose
+  // Local Database version
+  //.connect(process.env.DATABASE_LOCAL, {
+
+  // hosted database version
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    //console.log(con.connection);
+    console.log('DB connection successful!');
+  });
 
 //console.log(app.get('env'));
 //console.log(process.env);

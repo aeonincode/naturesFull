85c64c10d05236d5765a8208387570f2bbbe4786
aeonincode <khashayar.sasannia@hotmail.com>
2020-12-11 15:59:19 +0100

    create a simple tour model

--- server.js
+++ server.js
@@ -19,10 +19,31 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
+  //.then(() => console.log('DB connection successful!');)
   .then((con) => {
     //console.log(con.connection);
     console.log('DB connection successful!');
   });
+
+const tourSchema = new mongoose.Schema({
+  // pass schema as object
+  name: {
+    type: String,
+    // Validator
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+// Create Model from schema
+const Tour = mongoose.model('Tour', tourSchema);
 
 //console.log(app.get('env'));
 //console.log(process.env);

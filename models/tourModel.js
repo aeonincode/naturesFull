24efc16063bab54c1fe2fb31bb311a13b9067062
aeonincode <khashayar.sasannia@hotmail.com>
2020-12-12 20:51:25 +0100

    Modeling the tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,15 +7,57 @@ const tourSchema = new mongoose.Schema({
     // Validator
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    // trim : remove all white space in the beginning and end of the string
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    // name of the image,later we will be able to read from file system
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  // save images as array of strings
+  images: [String],
+  //a timestamp that is set by time that user gets a new tour, at tile tour created
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  // Array of dates, when tours starts
+  startDates: [Date],
 });
 
 // Create Model from schema

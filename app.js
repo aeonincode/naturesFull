0cc7f752b6d9d28a7b5496c0083f3a6cf0928f74
aeonincode <khashayar.sasannia@hotmail.com>
2020-12-25 17:26:23 +0100

    handling unhandled routes

--- app.js
+++ app.js
@@ -1,38 +1,44 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // MIDDLEWARES
 //onsole.log(process.env.NODE_ENV)
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-    //app.use(morgan('tiny'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+  //app.use(morgan('tiny'));
 }
-
 
 // simple middleware
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-    console.log('Hello from the middleware 🤘🏻');
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log('Hello from the middleware 🤘🏻');
+//     next();
+// });
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 // ROUTES
-
 // mounting the Route
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// all stand for all http methods
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;

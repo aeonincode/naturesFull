84131c823f4f12c7057e5bc8f6ee16538d1982a3
aeonincode <khashayar.sasannia@hotmail.com>
2020-12-29 18:49:30 +0100

    Creating New Users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 // create new Router and save this to new variable tourRouter
 const router = express.Router();
 
-router.route('/').get(userController.getAllUsers).post(userController.createUser);
-router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
+router.post('/signup', authController.signup);
+
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;

bdca9ae78c96ba1476e93f6f3f15cea61d16377b
aeonincode <khashayar.sasannia@hotmail.com>
2021-01-05 19:05:44 +0100

    Updating the current User Data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -19,6 +19,8 @@ router.patch(
   authController.updatePassword
 );
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 router
   .route('/')
   .get(userController.getAllUsers)

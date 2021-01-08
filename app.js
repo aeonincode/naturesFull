ac3d6e7c1ade5033957f85686f1cb575c7787ea5
aeonincode <khashayar.sasannia@hotmail.com>
2021-01-08 17:48:14 +0100

    implementing Rate Limiting fix bug

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -21,7 +22,7 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
-app.use('/app', limiter);
+app.use('/api', limiter);
 
 // simple middleware
 app.use(express.json());

f7a93ee68295f61d7e3545df819dbc0d3bb2fdf9
aeonincode <khashayar.sasannia@hotmail.com>
2021-01-11 19:31:03 +0100

    Creating and Getting Reviews

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -68,9 +69,10 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
-// mounting the Route
+// mounting the Route on path
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // all stand for all http methods
 app.all('*', (req, res, next) => {

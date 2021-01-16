ad41c6aad0d90eb278e283ddaed521f4f308d33a
aeonincode <khashayar.sasannia@hotmail.com>
2021-01-16 19:46:04 +0100

    Adding Missing Authentication and Authorization, fix role for admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,6 +50,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
+    role: req.body.role,
   });
 
   createSendToken(newUser, 201, res);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,21 +13,15 @@ router.post('/forgotPassword', authController.forgotPassword);
 // receive token as well as new password
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+// Protect all routes after this middleware
+router.use(authController.protect);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')

e144a44fac31480e66d1db6439efc23e9a043192
aeonincode <khashayar.sasannia@hotmail.com>
2021-01-17 16:44:30 +0100

    Improving read Performance with Indexes, tack back explain() method from handlerFactory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -83,7 +83,8 @@ exports.getAll = (Model) =>
       .sort()
       .limitFields()
       .paginate();
-    const doc = await features.query.explain();
+    // const doc = await features.query.explain();
+    const doc = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({

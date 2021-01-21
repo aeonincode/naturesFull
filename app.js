59e53168d5a10301c149d868fafa1a3fa642106d
aeonincode <khashayar.sasannia@hotmail.com>
2021-01-21 12:54:28 +0100

    First Steps with Pug

--- app.js
+++ app.js
@@ -76,7 +76,10 @@ app.use((req, res, next) => {
 // ROUTES
 // mounting the Route on path
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Xerxes',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);

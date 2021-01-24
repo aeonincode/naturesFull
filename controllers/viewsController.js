5024e773757e994f2e80929aeb4ccfba0a499105
aeonincode <khashayar.sasannia@hotmail.com>
2021-01-24 20:14:59 +0100

    Including a Map with Mapbox

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,18 +13,52 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
-  // 1) Get the data, for the requested tour (including reviews and guides)
+// exports.getTour = catchAsync(async (req, res) => {
+//   // 1) Get the data, for the requested tour (including reviews and guides)
+//   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+//     path: 'reviews',
+//     fields: 'review rating user',
+//   });
+//   // 2) Build template
+
+//   // 3) Render template using data from 1)
+
+//   res.status(200).render('tour', {
+//     title: `${tour.name} Tour`,
+//     tour,
+//   });
+// });
+
+// in order to show map
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
-    fields: 'review rating user',
+    fields: 'reviews rating user',
   });
-  // 2) Build template
 
-  // 3) Render template using data from 1)
-
-  res.status(200).render('tour', {
-    title: `${tour.name} Tour`,
-    tour,
-  });
+  if (!tour) {
+    return next(new AppError('There is no tour with that name', 404));
+  }
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('tour', {
+      title: `${tour.name}`,
+      tour,
+    });
 });
+
+exports.getLoginForm = (req, res) => {
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "connect-src 'self' https://cdnjs.cloudflare.com"
+    )
+    .render('login', {
+      title: 'User Login',
+    });
+};
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,51 @@
 /* eslint-disable */
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiYWVvbmluY29kZSIsImEiOiJja2tiZHlhMHIwMWliMm5xc3BjOWQ0a285In0.oc3DriXTZ7kFOu-MnLiqKg';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/aeonincode/ckkbepsiw0nqb17ps59w8deia',
+  scrollZoom: false,
+  //center: [-118.303089, 34.064741],
+  //zoom: 10,
+  //interactive: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add Popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
